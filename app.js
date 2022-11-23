3a067e05d83fe153f31c4a313cb82cd889b20d38
Azizbek Do'sanov <dusanovon1@gmail.com>
2022-11-23 15:09:58 +0500

    homework from js

---
+++ app.js
@@ -0,0 +1,12 @@
+let obj ={
+    name: "Azizbek",
+    surname: "Do'sanov",
+    age: "17",
+    study: "OWA",
+}
+
+console.log(obj)
+
+let prom = prompt("1-sonni kiriting ")
+let prom1 = prompt("2-sonni kiriting")
+console.log(Number(prom) % Number(prom1))
